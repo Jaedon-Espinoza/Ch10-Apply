dfea23a2fb153c4473a082eee159425b5a043ed7
Jaedon-Espinoza <jaedonespinoza2@gmail.com>
2024-12-05 11:29:20 -0500

    all code

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/* Name: Jaedon Espinoza
+   Date: 12/3/2024
+   File Name: script.js
+*/
+
+// Function to display content
+
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
